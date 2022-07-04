ca31e998e392f2bf5ba933bb0ee8d9655c306c33
Code_House <105477971+fns11@users.noreply.github.com>
2022-07-04 16:44:12 -0400

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -16,13 +16,7 @@ if uploaded_file is not None:
 
      # Can be used wherever a "file-like" object is accepted:
      dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
-st.markdown('''
-
-    Docs'''
-            
-           )
-
+ 
 
 
 st.header('Lets go')
